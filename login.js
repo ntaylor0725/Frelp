22cff115df96fa57b55bb4722dc199ba032c7b22
ntaylor0725 <79774521+ntaylor0725@users.noreply.github.com>
2026-01-30 22:21:56 -0500

    Remove Google login

--- login.js
+++ login.js
@@ -1,3 +1,8 @@
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        login();
+    }
+});
 function login() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
